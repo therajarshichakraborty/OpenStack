4c29bc745c6c3df6307fee0b1422a5878517aefa
Rajarshi Chakraborty <rajarshichakraborty2005@gmail.com>
2026-07-22 23:46:42 +0530

    add nestjs backend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.setGlobalPrefix("api")
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  )
+
+  app.enableCors({
+    rigin: process.env.WEB_URL ?? 'http://localhost:3001',
+    credentials: true,
+  })
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`API running on http://localhost:${port}/api`);
+
 }
 void bootstrap();
